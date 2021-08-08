e67926ee248bc40cd470bd031ff38fd4af26ab30
Juan Ignacio Rios <juanrios0495@gmail.com>
2021-08-08 19:18:49 -0300

    Delete obsolete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,17 +6,12 @@ import './ExpenseItem.css';
 const ExpenseItem = props => {
     const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle('Hola perro');
-    };
-
     return (
         <Card className="expense-item">
             <Expensedate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
-                <button onClick={clickHandler}>Change title</button>
             </div>
         </Card>
     );
