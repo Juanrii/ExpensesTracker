7d3a2c5f9c3264e49d9b63082c60c13c4579219c
Juanrii <55418564+Juanrii@users.noreply.github.com>
2021-08-13 18:17:03 -0300

    Remove State hook from Expense Items
    Not need it here.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Expensedate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
-    const [title, setTitle] = useState(props.title);
-
     return (
         <li>
             <Card className="expense-item">
                 <Expensedate date={props.date}/>
                 <div className="expense-item__description">
-                    <h2>{title}</h2>
+                    <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
             </Card>
